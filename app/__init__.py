7e6f72cf732754b2470f97a6e5e8d87ee314a0ef
Franco Prieto <fpa936@gmail.com>
2026-06-01 12:21:58 -0400

    chore: initial project structure

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_socketio import SocketIO
+
+socketio = SocketIO()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dev'
+    socketio.init_app(app)
+    return app
