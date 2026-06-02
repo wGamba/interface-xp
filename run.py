3aeee963d17648ff9c657688c82fdddb2bc71284
Franco Prieto <fpa936@gmail.com>
2026-06-02 06:51:58 -0400

    fix: allow Werkzeug dev server to run SocketIO

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
